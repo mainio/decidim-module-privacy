f8e694fa2d63e1ed97bcbc846bb5874f1955310d
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-23 15:50:18 +0300

    Hide the private messaging when the publicity id disabled

--- app/packs/src/decidim/privacy/decidim_privacy_settings.js
+++ app/packs/src/decidim/privacy/decidim_privacy_settings.js
@@ -1,7 +1,7 @@
 $(() => {
   let privacyMessagingText = document.getElementById("toggle-privacy-messaging");
   let PrivacyMessaging = document.getElementById("user_published_at");
-  if (PrivacyMessaging && PrivacyMessaging.value !== "1") {
+  if (PrivacyMessaging && PrivacyMessaging.value === "1") {
     privacyMessagingText.classList.add("hide");
   }
   const toggleVisibility = () => {
