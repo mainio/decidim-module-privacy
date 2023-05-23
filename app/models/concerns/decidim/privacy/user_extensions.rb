bc34ce8dab783b3eb18f91b715a0152a4c87c1d7
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-23 15:17:28 +0300

    Remove commented codes

--- app/models/concerns/decidim/privacy/user_extensions.rb
+++ app/models/concerns/decidim/privacy/user_extensions.rb
@@ -4,30 +4,10 @@ module Decidim
   module Privacy
     module UserExtensions
       extend ActiveSupport::Concern
-
-      class_methods do
-        # @see OrmAdapter::Base#find_first
-        # def find_first(options = {})
-        #   raise "FIND FIRST USER"
-        #   construct_relation(klass, options).first
-        # end
-
-        # def find_first_by_auth_conditions(tainted_conditions, opts = {})
-        #   raise "FIND FIRST"
-        #   to_adapter.find_first(devise_parameter_filter.filter(tainted_conditions).merge(opts))
-        # end
-
-        # def find_for_database_authentication(conditions)
-        #   raise conditions.inspect
-        #   find_for_authentication(conditions)
-        # end
-      end
-
       included do
+        default_scope { where.not(published_at: nil) }
         # we need to remove the default scope for the registeration, so as to check the uniqueness of
         # accounts through all of the accounts
-        default_scope { where.not(published_at: nil) }
-
         def self.find_for_authentication(warden_conditions)
           organization = warden_conditions.dig(:env, "decidim.current_organization")
           unscoped.find_by(
