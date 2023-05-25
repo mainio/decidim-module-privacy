55e5c6c17f28123f5d051f71db07c6097b7acbb9
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-25 16:51:01 +0300

    Exclude private authors from the proposals and collaborative proposals

---
+++ app/models/concerns/decidim/privacy/coauthorable_extensions.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Privacy
+    module CoauthorableExtensions
+      extend ActiveSupport::Concern
+      include Decidim::Coauthorable
+
+      included do
+        def authors
+          return @authors if defined?(@authors)
+
+          authors = coauthorships.pluck(:decidim_author_id, :decidim_author_type).each_with_object({}) do |(id, klass), all|
+            all[klass] ||= []
+            all[klass] << id
+          end
+          authors = authors.filter { |author| author.is_a?(Decidim::User) && !author.published_at.nil? }
+          return if authors.nil?
+
+          @authors = authors.flat_map do |klass, ids|
+            klass.constantize.where(id: ids)
+          end.compact.uniq
+        end
+      end
+    end
+  end
+end
