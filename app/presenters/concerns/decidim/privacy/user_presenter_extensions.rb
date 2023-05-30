43d4f3b8e3680a29cc8481cb498cff83e65410cc
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-30 10:49:33 +0300

    Override avatar methods for the private users

--- app/presenters/concerns/decidim/privacy/user_presenter_extensions.rb
+++ app/presenters/concerns/decidim/privacy/user_presenter_extensions.rb
@@ -20,6 +20,13 @@ module Decidim
           decidim.profile_url(__getobj__.nickname)
         end
 
+        def profile_path
+          return "" if respond_to?(:deleted?) && deleted?
+          return "" unless public_user?
+
+          decidim.profile_path(__getobj__.nickname)
+        end
+
         def avatar_url(variant = nil)
           return default_avatar_url if __getobj__.blocked?
           return default_avatar_url unless avatar.attached?
@@ -37,7 +44,7 @@ module Decidim
         private
 
         def public_user?
-          __getobj__.published_at.nil?
+          __getobj__.published_at.present?
         end
       end
     end
