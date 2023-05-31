6f063d3c2eaa06ddb9534881b23f46d476d1f6a9
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-31 16:03:10 +0300

    Remove unnecessary method over-rides from the user_presenter

--- app/presenters/concerns/decidim/privacy/user_presenter_extensions.rb
+++ app/presenters/concerns/decidim/privacy/user_presenter_extensions.rb
@@ -13,20 +13,6 @@ module Decidim
           "@#{__getobj__.nickname}"
         end
 
-        def profile_url
-          return "" if respond_to?(:deleted?) && deleted?
-          return "" unless public_user?
-
-          decidim.profile_url(__getobj__.nickname)
-        end
-
-        def profile_path
-          return "" if respond_to?(:deleted?) && deleted?
-          return "" unless public_user?
-
-          decidim.profile_path(__getobj__.nickname)
-        end
-
         def avatar_url(variant = nil)
           return default_avatar_url if __getobj__.blocked?
           return default_avatar_url unless avatar.attached?
