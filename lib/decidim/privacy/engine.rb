5a7f755fa80f29ff919880806948d2e77d0ec889
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-05-31 19:13:25 +0300

    Remove the console warning during the initializer

--- lib/decidim/privacy/engine.rb
+++ lib/decidim/privacy/engine.rb
@@ -41,6 +41,7 @@ module Decidim
 
       initializer "decidim_privacy.add_customizations", before: "decidim_comments.query_extensions" do
         ActiveSupport.on_load(:active_record) do
+          remove_const(:OrmAdapter) if const_defined?(:OrmAdapter)
           self::OrmAdapter = ::Decidim::Privacy::OrmAdapter
         end
 
