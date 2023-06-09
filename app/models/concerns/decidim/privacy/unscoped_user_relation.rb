c48b2cb46dfbc06e9d95f2c317cda5df460f127e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-06-09 13:27:40 +0300

    Fix issue with the user relation overrides
    The previous implementation did not consider the column name for
    the association which meant it broke for some of the associations
    that use `user_id` instead of `decidim_user_id` as the column
    name.

--- app/models/concerns/decidim/privacy/unscoped_user_relation.rb
+++ app/models/concerns/decidim/privacy/unscoped_user_relation.rb
@@ -5,13 +5,33 @@ module Decidim
     module UnscopedUserRelation
       extend ActiveSupport::Concern
 
-      included do
-        options = reflect_on_association(:user).options
-        if options.keys.include?(:optional)
-          belongs_to :user, -> { entire_collection }, foreign_key: "decidim_user_id", class_name: "Decidim::User", optional: options[:optional]
-        else
-          belongs_to :user, -> { entire_collection }, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+      class_methods do
+        private
+
+        # Modifies the existing association with the given name by adding the
+        # `entire_collection` scope to that association to make it possible to
+        # fetch user records through the association even if those users have
+        # not published their profile.
+        def privacy_scope_association_to_entire_collection(association_name)
+          reflection = _reflections[association_name.to_s]
+          raise "Undefined association on #{name}: #{association_name}" unless reflection
+
+          # Find the correct association builder class
+          association = reflection.association_class.name.split("::").last.sub(/Association$/, "")
+          builder = ActiveRecord::Associations::Builder.const_get(association)
+
+          # Redefine the association with the modified scope
+          _reflections[association_name.to_s] = builder.build(
+            self,
+            reflection.name,
+            -> { entire_collection },
+            reflection.options
+          )
         end
+      end
+
+      included do
+        privacy_scope_association_to_entire_collection(:user)
       end
     end
   end
