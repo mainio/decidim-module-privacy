8cd6deb811c14e92b195801fa9b492d558d475d5
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-06-27 10:32:22 +0300

    Refactor permittable examples

--- spec/shared/permittable_examples.rb
+++ spec/shared/permittable_examples.rb
@@ -6,13 +6,33 @@ shared_examples "permittable create actions" do
       sign_in user
     end
 
-    it "does not permit new action" do
-      get :new, params: params
-      expect(response).to render_template("decidim/privacy/privacy_block")
-    end
-
     it "does not permit create action" do
       post :create, params: params
+      expect(response).to render_template("decidim/privacy/privacy_block")
+    end
+  end
+
+  context "when public user" do
+    before do
+      user.update!(published_at: Time.current)
+      sign_in user
+    end
+
+    it "permits create action" do
+      post :create, params: params
+      expect(response).to have_http_status(:ok).or have_http_status(:no_content)
+    end
+  end
+end
+
+shared_examples "permittable new actions" do
+  context "when private user" do
+    before do
+      sign_in user
+    end
+
+    it "does not permit new action" do
+      get :new, params: params
       expect(response).to render_template("decidim/privacy/privacy_block")
     end
   end
@@ -28,11 +48,6 @@ shared_examples "permittable create actions" do
       expect(response).to render_template(:new)
       expect(response).to have_http_status(:ok)
     end
-
-    it "permits create action" do
-      post :create, params: params
-      expect(response).to have_http_status(:ok)
-    end
   end
 end
 
@@ -40,12 +55,6 @@ shared_examples "permittable update actions" do
   context "when private user" do
     before do
       sign_in user
-    end
-
-    it "does not permit edit action" do
-      get :edit, params: params
-      expect(response).to redirect_to("/")
-      expect(flash[:alert]).to be_present
     end
 
     it "does not permit update action" do
@@ -61,16 +70,37 @@ shared_examples "permittable update actions" do
       sign_in user
     end
 
+    it "permits update action" do
+      patch :update, params: params
+      expect(response).to have_http_status(:found)
+      expect(flash[:alert]).not_to be_present
+    end
+  end
+end
+
+shared_examples "permittable edit actions" do
+  context "when private user" do
+    before do
+      sign_in user
+    end
+
     it "does not permit edit action" do
+      get :edit, params: params
+      expect(response).to redirect_to("/")
+      expect(flash[:alert]).to be_present
+    end
+  end
+
+  context "when public user" do
+    before do
+      user.update!(published_at: Time.current)
+      sign_in user
+    end
+
+    it "permits edit action" do
       get :edit, params: params
       expect(response).to render_template(:edit)
       expect(response).to have_http_status(:ok)
-      expect(flash[:alert]).not_to be_present
-    end
-
-    it "does not permit update action" do
-      patch :update, params: params
-      expect(response).to have_http_status(:found)
       expect(flash[:alert]).not_to be_present
     end
   end
