2c6316e2a27ac8d5faade74540c49bdb40580cac
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-06-29 12:30:31 +0300

    Add tests for conversation_helper_extensions

---
+++ spec/helpers/concerns/conversation_helper_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Messaging
+    describe ConversationHelper do
+      describe "#conversation_label_for" do
+        let(:user) { create :user, :confirmed }
+        let(:participants) { [user] }
+
+        before do
+          helper.instance_variable_set(:@virtual_path, "decidim.messaging.conversations.show")
+        end
+
+        it "does not includes the user name for private users" do
+          expect(helper.conversation_label_for(participants)).to eq "Conversation with Private user"
+        end
+
+        context "when public user" do
+          let(:user) { create :user, :confirmed, published_at: Time.current }
+
+          it "includes the user name" do
+            expect(helper.conversation_label_for(participants)).to eq "Conversation with #{user.name} (@#{user.nickname})"
+          end
+        end
+
+        context "when user is deleted" do
+          let(:user) { create :user, :deleted }
+
+          it "doesn't include the user name" do
+            expect(helper.conversation_label_for(participants)).to eq "Conversation with Participant deleted"
+          end
+        end
+      end
+
+      describe "#username_list" do
+        let(:user) { create :user, :confirmed }
+        let(:participants) { [user] }
+
+        before do
+          helper.instance_variable_set(:@virtual_path, "decidim.messaging.conversations.show")
+        end
+
+        context "when private user" do
+          it "includes the user name" do
+            expect(helper.username_list(participants)).to eq "<span class=\"label label--small label--basic\">Private user</span>"
+          end
+        end
+
+        context "when public user" do
+          let(:user) { create :user, :confirmed, published_at: Time.current }
+
+          it "includes the user name" do
+            expect(helper.username_list(participants)).to eq "<strong>#{user.name}</strong>"
+          end
+        end
+
+        context "when user is deleted" do
+          let(:user) { create :user, :deleted }
+
+          it "doesn't include the user name" do
+            expect(helper.username_list(participants)).to eq "<span class=\"label label--small label--basic\">Participant deleted</span>"
+          end
+        end
+      end
+
+      describe "#conversation_name_for" do
+        let(:user) { create :user, :confirmed }
+        let(:participants) { [user] }
+
+        before do
+          helper.instance_variable_set(:@virtual_path, "decidim.messaging.conversations.show")
+        end
+
+        context "when user is public" do
+          let(:user) { create :user, :confirmed, published_at: Time.current }
+
+          it "includes the user name" do
+            expect(helper.conversation_name_for(participants)).to eq "<strong>#{user.name}</strong><br><span class=\"muted\">@#{user.nickname}</span>"
+          end
+        end
+
+        context "when private user" do
+          it "includes the user name" do
+            expect(helper.conversation_name_for(participants)).to eq "<span class=\"label label--small label--basic\">Private user</span><br><span class=\"muted\"><i>This user has decided to make their profile private. New messages to this conversation have been therefore disabled.</i></span>"
+          end
+        end
+
+        context "when user is deleted" do
+          let(:user) { create :user, :deleted }
+
+          it "doesn't include the user name" do
+            expect(helper.conversation_name_for(participants)).to eq "<span class=\"label label--small label--basic\">Participant deleted</span>"
+          end
+        end
+      end
+    end
+  end
+end
