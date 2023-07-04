90f5455461f404669440f436c858cd9fc28060f8
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-07-04 17:02:12 +0300

    Remove unnecessary files

--- lib/decidim/privacy.rb
+++ lib/decidim/privacy.rb
@@ -6,7 +6,6 @@ module Decidim
   # This namespace holds the logic of the `Privacy` component. This component
   # allows users to create privacy in a participatory space.
   module Privacy
-    autoload :PrivacyHelper, "decidim/privacy/privacy_helper"
     autoload :OrmAdapter, "decidim/privacy/orm_adapter"
     autoload :CommentSerializerExtensions, "decidim/privacy/comment_serializer_extensions"
 
