cdd459d6021aa09aeb61c7756913e10cd41228ef
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-08-05 12:54:39 +0300

    Configure simplecov using `.simplecov`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,6 @@
 
 require "decidim/dev"
 
-require "simplecov"
-SimpleCov.start
-
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 
 Decidim::Dev.dummy_app_path = File.expand_path(File.join("spec", "decidim_dummy_app"))
