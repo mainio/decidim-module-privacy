17acc3552ae557e1af7460f25a4e2972edd89d1c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-09-21 11:48:00 +0300

    Fix issue with linked proposals not displaying for private authors

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@
 require "decidim/privacy/test/factories"
 require "decidim/core/test/factories"
 require "decidim/participatory_processes/test/factories"
+require "decidim/accountability/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/meetings/test/factories"
 require "decidim/debates/test/factories"
