c1dcfbb3b4a4a103ebef26336f3ce242c233c097
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-02-14 16:27:09 +0200

    Add reference to the core bug fix PR

--- app/controllers/concerns/decidim/privacy/impersonatable_users_controller_extensions.rb
+++ app/controllers/concerns/decidim/privacy/impersonatable_users_controller_extensions.rb
@@ -2,6 +2,10 @@
 
 module Decidim
   module Privacy
+    # Fixes the following bug in the core:
+    # https://github.com/decidim/decidim/pull/12458
+    #
+    # TODO: Remove after the fix is released.
     module ImpersonatableUsersControllerExtensions
       extend ActiveSupport::Concern
 
