1303772a83c21563b7672182f55f700402f8582b
Joonas <joonas.aapro@mainiotech.fi>
2024-05-21 09:14:14 +0300

    Fix admin participant list email visibility

--- app/controllers/concerns/decidim/privacy/officializations_controller_extensions.rb
+++ app/controllers/concerns/decidim/privacy/officializations_controller_extensions.rb
@@ -10,6 +10,13 @@ module Decidim
         def collection
           @collection ||= current_organization.users.entire_collection.not_deleted.left_outer_joins(:user_moderation)
         end
+
+        def user
+          @user ||= Decidim::User.entire_collection.find_by(
+            id: params[:user_id],
+            organization: current_organization
+          )
+        end
       end
     end
   end
