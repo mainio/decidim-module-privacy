105942c5f09b1020edee3dcd0f026a6de1453ff4
Joonas <joonas.aapro@mainiotech.fi>
2024-05-21 07:55:54 +0300

    Fix privacy settings messaging JS

--- app/packs/src/decidim/privacy/decidim_privacy_settings.js
+++ app/packs/src/decidim/privacy/decidim_privacy_settings.js
@@ -1,12 +1,22 @@
 $(() => {
   let privacyMessagingText = document.getElementById("toggle-privacy-messaging");
-  let accountPublicity = document.getElementById("user_published_at");
+  let accountPublicity = document.getElementById("published_at");
 
   if (accountPublicity && accountPublicity.dataset.publicity === "true") {
-    privacyMessagingText.classList.remove("hide");
+    privacyMessagingText.style.display = "block";
+  } else {
+    console.log(accountPublicity.dataset)
+    privacyMessagingText.style.display = "none";
   }
+
   const toggleVisibility = () => {
-    privacyMessagingText.classList.toggle("hide");
+    if (privacyMessagingText.style.display === "none"){
+      privacyMessagingText.style.display = "block";
+      console.log("NÄYTÄ")
+    } else {
+      privacyMessagingText.style.display = "none";
+      console.log("PIILOTA")
+    }
   };
 
   accountPublicity.addEventListener("click", () => {
