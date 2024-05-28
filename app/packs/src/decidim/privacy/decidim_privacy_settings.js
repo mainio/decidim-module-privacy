78c54d1c876de0a1a8c90bb403620a410242a1c4
Joonas <joonas.aapro@mainiotech.fi>
2024-05-28 10:07:19 +0300

    Update author information when private

--- app/packs/src/decidim/privacy/decidim_privacy_settings.js
+++ app/packs/src/decidim/privacy/decidim_privacy_settings.js
@@ -5,17 +5,14 @@ $(() => {
   if (accountPublicity && accountPublicity.dataset.publicity === "true") {
     privacyMessagingText.style.display = "block";
   } else {
-    console.log(accountPublicity.dataset)
     privacyMessagingText.style.display = "none";
   }
 
   const toggleVisibility = () => {
     if (privacyMessagingText.style.display === "none"){
       privacyMessagingText.style.display = "block";
-      console.log("NÄYTÄ")
     } else {
       privacyMessagingText.style.display = "none";
-      console.log("PIILOTA")
     }
   };
 
--- lib/decidim/privacy/engine.rb
+++ lib/decidim/privacy/engine.rb
@@ -52,7 +52,7 @@ module Decidim
         end
       end
 
-      initializer "decidim_privacy.add_cells_view_paths", before: "decidim_comments.add_cells_view_paths" do
+      initializer "decidim_privacy.add_cells_view_paths", before: "decidim_core.add_cells_view_paths" do
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Privacy::Engine.root}/app/cells")
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Privacy::Engine.root}/app/views")
       end
