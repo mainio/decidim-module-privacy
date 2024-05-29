65ba8b4b2eca64d19d74fac4a7ddd9c8af2b8d61
Joonas <joonas.aapro@mainiotech.fi>
2024-05-29 11:11:05 +0300

    Update views

--- app/services/concerns/decidim/privacy/order_reminder_generator_extensions.rb
+++ app/services/concerns/decidim/privacy/order_reminder_generator_extensions.rb
@@ -10,12 +10,12 @@ module Decidim
 
         # Overridden in order to include all users, not only published accounts.
         def send_reminders(component)
-          budgets = Decidim::Budgets::Budget.where(component: component)
+          budgets = Decidim::Budgets::Budget.where(component:)
           pending_orders = Decidim::Budgets::Order.where(budget: budgets, checked_out_at: nil)
           users = Decidim::User.entire_collection.where(id: pending_orders.pluck(:decidim_user_id).uniq)
           users.each do |user|
-            reminder = Decidim::Reminder.find_or_create_by(user: user, component: component)
-            users_pending_orders = pending_orders.where(user: user)
+            reminder = Decidim::Reminder.find_or_create_by(user:, component:)
+            users_pending_orders = pending_orders.where(user:)
             update_reminder_records(reminder, users_pending_orders)
             if reminder.records.active.any?
               Decidim::Budgets::SendVoteReminderJob.perform_later(reminder)
