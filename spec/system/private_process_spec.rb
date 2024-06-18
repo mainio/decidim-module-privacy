803fdd4e25cc25d0deef8031f5355d666c246a3a
Joonas <joonas.aapro@mainiotech.fi>
2024-06-18 08:17:38 +0300

    Add new private process spec

--- spec/system/private_process_spec.rb
+++ spec/system/private_process_spec.rb
@@ -2,14 +2,14 @@
 
 require "spec_helper"
 
-describe "Private Participatory Processes", type: :system do
+describe "Private Participatory Processes" do
   let!(:organization) { create(:organization) }
-  let!(:participatory_process) { create :participatory_process, :published, organization: organization }
-  let!(:private_participatory_process) { create :participatory_process, :published, organization: organization, private_space: true }
-  let!(:private_user) { create :user, :confirmed, organization: organization }
-  let!(:participatory_space_private_user) { create :participatory_space_private_user, user: private_user, privatable_to: private_participatory_process }
+  let!(:participatory_process) { create(:participatory_process, :published, organization:) }
+  let!(:private_participatory_process) { create(:participatory_process, :published, organization:, private_space: true) }
+  let!(:private_user) { create(:user, :confirmed, organization:) }
+  let!(:participatory_space_private_user) { create(:participatory_space_private_user, user: private_user, privatable_to: private_participatory_process) }
 
-  context "when user is logged in and is a \"private participatory space\" -user and also a private user" do
+  context "when user is logged in and is a \"private participatory process\" -user and also a private user" do
     before do
       switch_to_host(organization.host)
       login_as private_user, scope: :user
@@ -33,6 +33,22 @@ describe "Private Participatory Processes", type: :system do
 
       expect(page).to have_current_path decidim_participatory_processes.participatory_process_path(private_participatory_process)
       expect(page).to have_content "This is a private process"
+    end
+  end
+
+  context "when listing private participatory process private users and user is a private user" do
+    let!(:admin) { create(:user, :admin, :confirmed, organization:) }
+
+    before do
+      switch_to_host(organization.host)
+      login_as admin, scope: :user
+      visit decidim_admin_participatory_processes.edit_participatory_process_path(private_participatory_process)
+      find("a[href*='participatory_space_private_users']").click
+    end
+
+    it "shows user in the list" do
+      expect(page).to have_content(private_user.name)
+      expect(page).to have_content(private_user.email)
     end
   end
 end
