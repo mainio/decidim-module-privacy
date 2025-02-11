4497ebde7090928d8a025afbd7e0d49565f479bb
Joonas <joonas.aapro@mainiotech.fi>
2025-02-11 12:55:56 +0200

    Fix resource blank error when rejecting usergroups

--- app/models/concerns/decidim/privacy/action_log_extensions.rb
+++ app/models/concerns/decidim/privacy/action_log_extensions.rb
@@ -5,22 +5,16 @@ module Decidim
     module ActionLogExtensions
       extend ActiveSupport::Concern
 
-      include Decidim::Privacy::UnscopedUserRelation
-
       included do
         # In case the resource happens to be a user, it has to be unscoped for
         # the resource to be found. Otherwise when trying to save such ActionLog
         # record, the validations would fail.
-        belongs_to :resource,
-                   lambda {
-                     if klass == Decidim::User || klass == Decidim::UserBaseEntity
-                       entire_collection
-                     else
-                       self
-                     end
-                   },
-                   polymorphic: true,
-                   optional: true
+
+        def resource
+          return super unless ["Decidim::UserGroup", "Decidim::User", "Decidim::UserBaseEntity"].include?(resource_type)
+
+          Decidim::UserGroup.entire_collection.find_by(id: resource_id)
+        end
       end
     end
   end
