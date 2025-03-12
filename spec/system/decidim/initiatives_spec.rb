e4350892feb82ad3ef4621513b2ba2353642e5d7
Joonas <joonas.aapro@mainiotech.fi>
2025-03-12 15:43:32 +0200

    Add initiatives spec

--- spec/system/decidim/initiatives_spec.rb
+++ spec/system/decidim/initiatives_spec.rb
@@ -4,7 +4,10 @@ require "spec_helper"
 
 describe "Initiatives", type: :system do
   let!(:organization) { create(:organization) }
+  let!(:initiative) { create(:initiative, :created, author: user, organization: organization) }
   let!(:user) { create(:user, :confirmed, organization: organization) }
+  let!(:authorization) { create(:authorization, :granted, user: user) }
+  let!(:initiatives_type) { create(:initiatives_type, organization: organization) }
 
   before do
     switch_to_host(organization.host)
@@ -12,42 +15,12 @@ describe "Initiatives", type: :system do
     visit decidim.root_path
   end
 
-  context "when trying to create a new initiative" do
-    let!(:initiative) { create(:initiative, :created, author: user, organization: organization) }
-    let!(:user) { create(:user, :confirmed, organization: organization) }
-    let!(:authorization) { create(:authorization, :granted, user: user) }
-    let!(:initiatives_type) { create(:initiatives_type, organization: organization) }
-
-    context "when user private" do
-      it "renders a site that tells user to publish your account" do
-        visit decidim_initiatives.initiatives_path
-        click_link "New initiative"
-        expect(page).to have_content("You are trying to access a page which requires your profile to be public.")
-      end
-    end
-
-    context "when user public" do
-      let!(:user) { create(:user, :confirmed, :published, organization: organization) }
-
-      it "renders the site to create a new initiative" do
-        visit decidim_initiatives.initiatives_path
-        click_link "New initiative"
-
-        expect(page).to have_content("I want to create a #{initiatives_type.title["es"]}")
-        expect(page).to have_content("I want to promote this initiative")
-      end
-    end
-
-    context "when user tries to edit initiative" do
+  context "when anonymity disabled" do
+    context "when trying to create a new initiative" do
       context "when user private" do
         it "renders a site that tells user to publish your account" do
           visit decidim_initiatives.initiatives_path
-
-          find("label", text: "My initiatives").click
-          uncheck("Open")
-          click_link initiative.title["en"]
-          click_link "Edit"
-
+          click_link "New initiative"
           expect(page).to have_content("You are trying to access a page which requires your profile to be public.")
         end
       end
@@ -55,30 +28,100 @@ describe "Initiatives", type: :system do
       context "when user public" do
         let!(:user) { create(:user, :confirmed, :published, organization: organization) }
 
-        it "renders the site to edit initiative" do
+        it "renders the site to create a new initiative" do
+          visit decidim_initiatives.initiatives_path
+          click_link "New initiative"
+
+          expect(page).to have_content("I want to create a #{initiatives_type.title["es"]}")
+          expect(page).to have_content("I want to promote this initiative")
+        end
+      end
+
+      context "when user tries to edit initiative" do
+        context "when user private" do
+          it "renders a site that tells user to publish your account" do
+            visit decidim_initiatives.initiatives_path
+
+            find("label", text: "My initiatives").click
+            uncheck("Open")
+            click_link initiative.title["en"]
+            click_link "Edit"
+
+            expect(page).to have_content("You are trying to access a page which requires your profile to be public.")
+          end
+        end
+
+        context "when user public" do
+          let!(:user) { create(:user, :confirmed, :published, organization: organization) }
+
+          it "renders the site to edit initiative" do
+            visit decidim_initiatives.initiatives_path
+
+            find("label", text: "My initiatives").click
+            uncheck("Open")
+            click_link initiative.title["en"]
+            click_link "Edit"
+
+            expect(page).to have_content("EDIT INITIATIVE")
+          end
+        end
+      end
+
+      context "when initiative is done by private user" do
+        let!(:initiative) { create(:initiative, author: user, organization: organization) }
+
+        it "doesn't show author name" do
           visit decidim_initiatives.initiatives_path
 
-          find("label", text: "My initiatives").click
-          uncheck("Open")
-          click_link initiative.title["en"]
-          click_link "Edit"
-
-          expect(page).to have_content("EDIT INITIATIVE")
+          within ".card--initiative", match: :first do
+            expect(page).not_to have_content(user.name)
+            expect(page).to have_content("Unnamed participant")
+          end
         end
       end
     end
+  end
 
-    context "when initiative is done by private user" do
-      let!(:initiative) { create(:initiative, author: user, organization: organization) }
+  context "when anonymity enabled", :anonymity do
+    let!(:user) { create(:user, :confirmed, organization: organization) }
 
-      it "doesn't show author name" do
-        visit decidim_initiatives.initiatives_path
-
-        within ".card--initiative", match: :first do
-          expect(page).not_to have_content(user.name)
+    context "when trying to create a new initiative" do
+      context "when user anonymous" do
+        it "renders a site that tells user to publish your account" do
+          visit decidim_initiatives.initiatives_path
+          click_link "New initiative"
+          expect(page).to have_content("Your profile is anonymous")
         end
+      end
 
-        expect(page).not_to have_selector(".author-data")
+      context "when user tries to edit initiative" do
+        context "when user set as anonymous" do
+          let!(:user) { create(:user, :anonymous, :confirmed, organization: organization) }
+
+          it "renders the site to edit initiative" do
+            visit decidim_initiatives.initiatives_path
+
+            find("label", text: "My initiatives").click
+            uncheck("Open")
+            click_link initiative.title["en"]
+            click_link "Edit"
+
+            expect(page).to have_content("EDIT INITIATIVE")
+          end
+        end
+      end
+
+      context "when initiative is done by anonymous user" do
+        let!(:initiative) { create(:initiative, author: user, organization: organization) }
+
+        it "doesn't show author name" do
+          visit decidim_initiatives.initiatives_path
+
+          within ".card--initiative", match: :first do
+            expect(page).not_to have_content(user.name)
+            expect(page).to have_content("Unnamed participant")
+          end
+        end
       end
     end
   end
