c57d160e79c6b30b19119b33915d4775c0dd70f2
Joonas <joonas.aapro@mainiotech.fi>
2025-09-30 08:59:42 +0300

    Use no scope for participatory space user if entire_collection doesn't exist

--- lib/decidim/participatory_space_user.rb
+++ lib/decidim/participatory_space_user.rb
@@ -11,7 +11,11 @@ module Decidim
     included do
       validate :user_and_space_same_organization
 
-      belongs_to :user, -> { entire_collection }, foreign_key: "decidim_user_id", class_name: "Decidim::User", optional: true
+      belongs_to :user,
+                 (Decidim::User.respond_to?(:entire_collection) ? -> { entire_collection } : nil),
+                 foreign_key: "decidim_user_id",
+                 class_name: "Decidim::User",
+                 optional: true
 
       ransacker :name do
         Arel.sql(%{("decidim_users"."name")::text})
