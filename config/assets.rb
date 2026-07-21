8fd8cfa1d83fa87e6c9d39edf3f33ec86f44769c
Joonas <joonas.aapro@mainiotech.fi>
2026-07-21 08:49:17 +0300

    Update asset -methods to use Shakapacker namespace instead of webpacker

--- config/assets.rb
+++ config/assets.rb
@@ -2,11 +2,11 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_path("#{base_path}/app/packs")
+Decidim::Shakapacker.register_entrypoints(
   decidim_privacy: "#{base_path}/app/packs/entrypoints/decidim_privacy.js",
   decidim_account_publish_handler: "#{base_path}/app/packs/entrypoints/decidim_account_publish_handler.js",
   decidim_privacy_settings: "#{base_path}/app/packs/entrypoints/decidim_privacy_settings.js",
   decidim_anonymity_settings: "#{base_path}/app/packs/entrypoints/decidim_anonymity_settings.js"
 )
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/privacy/privacy")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/privacy/privacy")
