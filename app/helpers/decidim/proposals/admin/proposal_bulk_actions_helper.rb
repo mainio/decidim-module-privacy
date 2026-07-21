042a023df9d7eb3eff3734ebd06cff168368e6d0
Joonas <joonas.aapro@mainiotech.fi>
2026-07-21 08:49:43 +0300

    Update proposal bulk actions -methods

--- app/helpers/decidim/proposals/admin/proposal_bulk_actions_helper.rb
+++ app/helpers/decidim/proposals/admin/proposal_bulk_actions_helper.rb
@@ -8,31 +8,44 @@ module Decidim
           Decidim::Proposals::Proposal.find(id)
         end
 
-        # Public: Generates a select field with the valuators of the given participatory space.
+        # Public: Generates a select field with the templates of the given component.
         #
-        # participatory_space - A participatory space instance.
+        # component - A component instance.
         # prompt - An i18n string to show as prompt
         #
         # Returns a String.
-        def bulk_valuators_select(participatory_space, prompt)
-          options_for_select = find_valuators_for_select(participatory_space)
-          select(:valuator_role, :id, options_for_select, prompt:)
+        def bulk_templates_select(component, prompt, id: nil)
+          options_for_select = find_templates_for_select(component)
+          select(:template, :template_id, options_for_select, prompt:, id:)
         end
 
-        # Internal: A method to cache to queries to find the valuators for the
-        # current space.
-        def find_valuators_for_select(participatory_space, current_user)
-          valuator_roles = participatory_space.user_roles(:valuator).order_by_name
-          valuators = Decidim::User.entire_collection.where(id: valuator_roles.pluck(:decidim_user_id)).to_a
+        def find_templates_for_select(component)
+          return [] unless Decidim.module_installed? :templates
+          return @templates_for_select if @templates_for_select
 
-          filtered_valuator_roles = valuator_roles.filter do |role|
+          templates = Decidim::Templates::Template.where(
+            target: :proposal_answer,
+            templatable: component
+          ).order(:templatable_id)
+
+          @templates_for_select = templates.map do |template|
+            [translated_attribute(template.name), template.id]
+          end
+        end
+
+        # find the evaluators for the current space.
+        def find_evaluators_for_select(participatory_space, current_user)
+          evaluator_roles = participatory_space.user_roles(:evaluator).order_by_name
+          evaluators = Decidim::User.entire_collection.where(id: evaluator_roles.pluck(:decidim_user_id)).to_a
+
+          filtered_evaluator_roles = evaluator_roles.filter do |role|
             role.decidim_user_id != current_user.id
           end
 
-          filtered_valuator_roles.map do |role|
-            valuator = valuators.find { |user| user.id == role.decidim_user_id }
+          filtered_evaluator_roles.map do |role|
+            evaluator = evaluators.find { |user| user.id == role.decidim_user_id }
 
-            [valuator.name, role.id]
+            [evaluator.name, role.id]
           end
         end
       end
