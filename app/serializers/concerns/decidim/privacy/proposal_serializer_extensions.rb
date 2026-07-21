5152495412cc29a6cef8bc84f507b249348eab20
Joonas <joonas.aapro@mainiotech.fi>
2026-07-21 20:21:36 +0300

    Delete extra override file
    This file already has an engine extension so it's useless, customized methods are moved to the extension file with this commit

--- app/serializers/concerns/decidim/privacy/proposal_serializer_extensions.rb
+++ app/serializers/concerns/decidim/privacy/proposal_serializer_extensions.rb
@@ -8,8 +8,8 @@ module Decidim
       included do
         private
 
-        def user_endorsements
-          proposal.endorsements.for_listing.map do |identity|
+        def user_likes
+          proposal.likes.for_listing.map do |identity|
             if identity.normalized_author.is_a?(Decidim::User) && identity.normalized_author.published_at.nil?
               private = Decidim::Privacy::PrivateUser.new
 
@@ -17,6 +17,28 @@ module Decidim
             else
               identity.normalized_author&.name
             end
+          end
+        end
+
+        def author_name(author)
+          if author.deleted?
+            ""
+          elsif author.respond_to?(:name)
+            translated_attribute(author.name) # is a Decidim::User or Decidim::Organization
+          elsif author.respond_to?(:title)
+            translated_attribute(author.title) # is a Decidim::Meetings::Meeting
+          end
+        end
+
+        def author_url(author)
+          if author.deleted?
+            ""
+          elsif author.respond_to?(:nickname)
+            profile_url(author) # is a Decidim::User
+          elsif author.respond_to?(:title)
+            meeting_url(author) # is a Decidim::Meetings::Meeting
+          else
+            root_url # is a Decidim::Organization
           end
         end
       end
